9a51e700f89d58f72a8d5b26114408a893858ab2
Puzzl3d3d <89996835+Puzzl3d3d@users.noreply.github.com>
2023-08-25 15:02:23 +0100

    Update defaultcpp.sh

--- defaultcpp.sh
+++ defaultcpp.sh
@@ -5,4 +5,6 @@ git clone https://github.com/Puzzl3d3d/default-cpp-project
 mv ./default-cpp-project/.* ./
 mv ./default-cpp-project/* ./
 
+rm defaultcpp.sh # This script is inside the repo, but we do not need it.
+
 sudo rm -r default-cpp-project 
