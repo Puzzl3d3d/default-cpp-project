469a3cfde7b3b39957cd72ec4ec29f912348b1b4
Puzzl3d3d <89996835+Puzzl3d3d@users.noreply.github.com>
2023-08-25 14:59:58 +0100

    Create defaultcpp.sh

---
+++ defaultcpp.sh
@@ -0,0 +1,8 @@
+sudo rm -r default-cpp-project 
+
+git clone https://github.com/Puzzl3d3d/default-cpp-project
+
+mv ./default-cpp-project/.* ./
+mv ./default-cpp-project/* ./
+
+sudo rm -r default-cpp-project 
